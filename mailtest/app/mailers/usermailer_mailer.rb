3ac7f65b61ce560636abba66c2f548554db7da08
Nagarani <nagarani@agiratech.com>
2016-11-25 14:17:36 +0530

    this sending email through rails

---
+++ mailtest/app/mailers/usermailer_mailer.rb
@@ -0,0 +1,11 @@
+class UsermailerMailer < ApplicationMailer
+ default from: 'nagaranigorantla@gmail.com'
+   
+   def welcome_email(user)
+      @user = user
+      @url  = 'http://www.gmail.com'
+      mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+   end
+   
+
+end
---
+++ mailtest/config/environment.rb
@@ -0,0 +1,10 @@
+# Load the Rails application.
+require_relative 'application'
+
+# Initialize the Rails application.
+Rails.application.initialize!
+
+
+
+
+# ActionMailer::Base.default_content_type = "text/html"
